883794359e792df3f4936acca98b8117b165fdde
v_olnyang <v_olnyang@tencent.com>
2022-05-26 19:25:19 +0800

    配置commit-msg

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
+  dest: './docs/dist',
   themeConfig: {
     sidebar: [
-      '/'
-      ,{
+      {
         title: 'Group 1',   // 必要的
         path: '/a/b',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
         collapsable: false, // 可选的, 默认值是 true,
@@ -18,7 +18,7 @@ module.exports = {
     displayAllHeaders: true, // 默认值：false
     lastUpdated: 'Last Updated', // string | boolean
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'vuejs/vuepress',
+    repo: 'https://github.com/LiunanYang/blog',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: '查看源码',
